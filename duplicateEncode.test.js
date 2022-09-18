4bdb55447fd76ea1df945a29ea49f62e062f44a0
S-Spiegl <steven.spiegl@gmail.com>
2022-09-19 00:12:10 +0100

    works for words whose letters only appear once

--- duplicateEncode.test.js
+++ duplicateEncode.test.js
@@ -4,4 +4,7 @@ describe('duplicateEncode', () => {
   it('converts a letter to ( if it appears once', () => {
     expect(duplicateEncode('d')).toEqual('(')
   })
+  it('works for words whose letters appear once', () => {
+    expect(duplicateEncode('din')).toEqual('(((')
+  })
 })
